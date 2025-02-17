f45164c512de44184fa9ee31d3aa9e50d44c0f0b
gretafg <gretafg@al.insper.edu.br>
2025-02-17 12:56:46 -0300

    kiss.c

--- main_isr_kiss.c
+++ main_isr_kiss.c
@@ -1,33 +1,35 @@
 #include "asf.h"
 
-int g_cnt = 0;
-char g_str[10];
-int batata;
+int volatile batata;
 
 // This code creates a progress bar on an OLED screen that
 // increases when the button is pressed.
 void btn_callback(void) {
-  printf("btn pressed \n");
-
-  if (g_cnt >= 8)
-    g_cnt = 0;
-
-  g_cnt = 2;
-
-  int i = 0;
-  for (i = 0; i < g_cnt; i++) {
-    g_str = 1;
-    g_str[i] = '*';
-    g_str[i + 1] = NULLL;
-    delay_ms(50);
-    gfx_mono_draw_string(g_str, 0, 0, &sysfont);
-  }
+  batata = 1;
 }
 
 void main(void) {
   // ...
-
-  batata = batata + 1;
+  int g_cnt = 0;
+  char g_str[10];
+  
   while (1) {
+    if (batata>0){
+      printf("btn pressed \n");
+      g_cnt++;
+    }
+  
+    if (g_cnt >= 8){
+      g_cnt = 0;
+    }
+  
+    int i = 0;
+    for (i = 0; i < g_cnt; i++) {
+      g_str = 1;
+      g_str[i] = '*';
+      g_str[i + 1] = NULLL;
+      delay_ms(50);
+      gfx_mono_draw_string(g_str, 0, 0, &sysfont);
+    }
   }
 }
